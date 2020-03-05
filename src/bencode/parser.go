117c72e186958bbd29312fb902585313ebc79dcd
Patanouk <jean.lainee@gmail.com>
2020-03-05 18:02:46 +0800

    Add function to parse integer from bencode format

--- src/bencode/parser.go
+++ src/bencode/parser.go
@@ -2,11 +2,37 @@ package bencode
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strconv"
 )
 
 const separator byte = ':'
+const endSeparator byte = 'e'
+
+func decodeInteger(reader *bufio.Reader) (int64, error) {
+	iByte, err := reader.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+
+	if iByte != 'i' {
+		return 0, fmt.Errorf("expected i as first rune when parsing integer, got %c", iByte)
+	}
+
+	slice, err := reader.ReadSlice(endSeparator)
+	if err != nil {
+		return 0, err
+	}
+
+	//TODO Verify slice non empty
+	intResult, err := strconv.ParseInt(string(slice[:len(slice)-1]), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return intResult, nil
+}
 
 func decodeString(reader *bufio.Reader) (string, error) {
 	slice, err := reader.ReadSlice(separator)
--- src/bencode/parser_test.go
+++ src/bencode/parser_test.go
@@ -43,3 +43,38 @@ func Test_decodeString(t *testing.T) {
 		})
 	}
 }
+
+func Test_decodeInteger(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int64
+		wantErr bool
+	}{
+		{"Input empty", args{input: ""}, 0, true},
+		{"Input integer only", args{input: "10"}, 0, true},
+		{"Input no end char", args{input: "i10"}, 0, true},
+		{"Input no start char", args{input: "10e"}, 0, true},
+		{"Input invalid integer", args{input: "10e"}, 0, true},
+
+		{"Input 0", args{input: "i0e"}, 0, false},
+		{"Input 10", args{input: "i10e"}, 10, false},
+		{"Input 99", args{input: "i99e"}, 99, false},
+		{"Input -1", args{input: "i-1e"}, -1, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := decodeInteger(bufio.NewReader(strings.NewReader(tt.args.input)))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("decodeInteger() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("decodeInteger() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
