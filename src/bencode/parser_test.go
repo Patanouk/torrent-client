aa03cda72d4d63041abc13615cf621050b4b8729
Patanouk <jean.lainee@gmail.com>
2020-03-06 11:38:06 +0800

    Cleanup tests name

--- src/bencode/parser_test.go
+++ src/bencode/parser_test.go
@@ -16,19 +16,19 @@ func Test_decodeString(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"Input empty", args{input: ""}, "", true},
-		{"Input no separator", args{input: "aa"}, "", true},
-		{"Input integer only", args{input: "4"}, "", true},
-		{"Input empty string after semicolon", args{input: "4:"}, "", true},
-		{"Input length not integer", args{input: "-aa:"}, "", true},
-		{"Input negative length", args{input: "-4:"}, "", true},
-		{"Input length too long", args{input: "4:aa"}, "", true},
+		{"empty", args{input: ""}, "", true},
+		{"no separator", args{input: "aa"}, "", true},
+		{"integer only", args{input: "4"}, "", true},
+		{"empty string after semicolon", args{input: "4:"}, "", true},
+		{"length not integer", args{input: "-aa:"}, "", true},
+		{"negative length", args{input: "-4:"}, "", true},
+		{"length too long", args{input: "4:aa"}, "", true},
 
-		{"Input zero length", args{input: "0:"}, "", false},
-		{"Input one length", args{input: "1:a"}, "a", false},
-		{"Input seven length", args{input: "7:abcdefg"}, "abcdefg", false},
+		{"zero length", args{input: "0:"}, "", false},
+		{"one length", args{input: "1:a"}, "a", false},
+		{"seven length", args{input: "7:abcdefg"}, "abcdefg", false},
 		//Invalid format globally, but valid in the scope of the function
-		{"Input length too short", args{input: "3:abcj"}, "abc", false},
+		{"length too short", args{input: "3:abcj"}, "abc", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -54,16 +54,16 @@ func Test_decodeInteger(t *testing.T) {
 		want    int64
 		wantErr bool
 	}{
-		{"Input empty", args{input: ""}, 0, true},
-		{"Input integer only", args{input: "10"}, 0, true},
-		{"Input no end char", args{input: "i10"}, 0, true},
-		{"Input no start char", args{input: "10e"}, 0, true},
-		{"Input invalid integer", args{input: "10e"}, 0, true},
+		{"empty", args{input: ""}, 0, true},
+		{"integer only", args{input: "10"}, 0, true},
+		{"no end char", args{input: "i10"}, 0, true},
+		{"no start char", args{input: "10e"}, 0, true},
+		{"invalid integer", args{input: "10e"}, 0, true},
 
-		{"Input 0", args{input: "i0e"}, 0, false},
-		{"Input 10", args{input: "i10e"}, 10, false},
-		{"Input 99", args{input: "i99e"}, 99, false},
-		{"Input -1", args{input: "i-1e"}, -1, false},
+		{"0", args{input: "i0e"}, 0, false},
+		{"10", args{input: "i10e"}, 10, false},
+		{"99", args{input: "i99e"}, 99, false},
+		{"-1", args{input: "i-1e"}, -1, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
